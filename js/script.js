5741a5fb4cbb980b2eaae0ff0326fefed435ecb3
Sourab Hossain <sourabhossain.fci@gmail.com>
2020-08-16 19:21:20 +0600

    add deleteButton

--- js/script.js
+++ js/script.js
@@ -135,3 +135,8 @@ allClearButton.addEventListener('click', button => {
     calculator.clear();
     calculator.updateDisplay();
 })
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete();
+    calculator.updateDisplay();
+})
