c5ffec991125b344a4ead49a058a669ffbe660d7
Sourab Hossain <sourabhossain.fci@gmail.com>
2020-08-16 18:16:20 +0600

    add calculator class

---
+++ js/script.js
@@ -0,0 +1,7 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.clear();
+    }
+}
